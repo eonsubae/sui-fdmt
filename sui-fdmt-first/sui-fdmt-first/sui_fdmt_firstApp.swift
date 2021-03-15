b7d0cacb1cc2d3550ed379be74be482c35cc3611
eonsubae <eonsubae@naver.com>
2021-03-15 23:20:38 +0900

    Introduction

---
+++ sui-fdmt-first/sui-fdmt-first/sui_fdmt_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  sui_fdmt_firstApp.swift
+//  sui-fdmt-first
+//
+//  Created by KRITSSEAN on 2021/03/15.
+//
+
+import SwiftUI
+
+@main
+struct sui_fdmt_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
