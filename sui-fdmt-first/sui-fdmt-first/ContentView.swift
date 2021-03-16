4dc788d80f8580d7b4b1595d65118ade6e24d61c
eonsubae <eonsubae@naver.com>
2021-03-16 22:40:31 +0900

    Modifiers

--- sui-fdmt-first/sui-fdmt-first/ContentView.swift
+++ sui-fdmt-first/sui-fdmt-first/ContentView.swift
@@ -4,7 +4,16 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "swift")
+                .resizable()
+                .frame(width: 100.0, height: 100.0)
+                .padding()
+                .background(Color.orange)
+                .padding([.leading, .bottom, .trailing])
+            
             Text("Howdy, world!")
+                .fontWeight(.bold)
+                .kerning(5.0)
+                .padding()
         }
     }
 }
