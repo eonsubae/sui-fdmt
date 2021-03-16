1487b25f69be3de436d28ef572aa859404ddffcb
eonsubae <eonsubae@naver.com>
2021-03-16 23:56:54 +0900

    State & Binding

---
+++ StateAndBinding/StateAndBinding/ContentView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var swiftyColor: Color = .red
+    @State private var swiftyOpacity: Double = 0.7
+    
+    var body: some View {
+        VStack {
+            SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
+            
+            
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .padding(25)
+                .foregroundColor(.white)
+                .opacity(swiftyOpacity)
+                .background(swiftyColor)
+                .cornerRadius(50)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct SwiftyControls: View {
+    @Binding var swiftyColor: Color
+    @Binding var swiftyOpacity: Double
+    
+    var body: some View {
+        ColorPicker("Swifty Color", selection: $swiftyColor)
+        Slider(value: $swiftyOpacity, in: 0...1)
+            .accentColor(swiftyColor)
+    }
+}
---
+++ StateAndBinding/StateAndBinding/StateAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndBindingApp.swift
+//  StateAndBinding
+//
+//  Created by KRITSSEAN on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct StateAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
