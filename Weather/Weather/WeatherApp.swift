17a613c319b0cd4b04047c14ce545ae8aafcffae
eonsubae <eonsubae@naver.com>
2021-03-19 01:01:11 +0900

    Stacks & ForEach

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by KRITSSEAN on 2021/03/19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
