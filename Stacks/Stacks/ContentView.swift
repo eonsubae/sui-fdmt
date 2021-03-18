4e55a96650f44a91b835fe3876f0dea1e97029d7
eonsubae <eonsubae@naver.com>
2021-03-18 23:18:31 +0900

    Repeat Views with ForEach

--- Stacks/Stacks/ContentView.swift
+++ Stacks/Stacks/ContentView.swift
@@ -1,116 +1,111 @@
-//
-//  ContentView.swift
-//  Stacks
-//
-//  Created by KRITSSEAN on 2021/03/17.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let mix = MeowMix()
+    
     var body: some View {
         VStack {
-            HStack(alignment: .bottom, spacing: 20.0) {
-                Thumbnail()
-                
-                VStack {
-                    MixDescription()
-                    Buttons()
-                }
-            }
-            .padding()
-            .frame(minHeight: 150, maxHeight: 250)
+            MeowMixHeader()
+                .padding()
             
-            Spacer()
+            Divider()
+                .padding()
+            
+            // Add a List of tracks
+            List(mix.tracks) { track in
+                TrackRow(track: track)
+                
+            }
+            
+            // Add FeaturedCats view
+            FeaturedCats(artists: mix.tracks.map(\.artist))
+                .padding(.vertical)
+                .background(Color.gray.opacity(0.2))
         }
     }
+}
+
+struct FeaturedCats: View {
+  let artists: [String]
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Featured Cats")
+        .font(.title)
+        .padding(.leading)
+      
+        ScrollView(.horizontal) {
+            HStack(alignment: .top, spacing: 25) {
+              ForEach(artists, id: \.self) { artist in
+                    FeaturedArtist(artist: artist)
+              }
+            }
+            .padding(.horizontal)
+        }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-struct Thumbnail: View {
-    var body: some View {
-        ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
-            ZStack {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.accentColor)
-                    .aspectRatio(1, contentMode: .fit)
-                    .shadow(radius: 10)
-                
-                Image(systemName: "play.fill")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .foregroundColor(.white)
-                    .opacity(0.4)
-            }
-            
-            VStack(alignment: .leading) {
-                Text("Meow!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                Text("Mix")
-                    .font(.largeTitle)
-                
-                Spacer()
-            }
-            .foregroundColor(.white)
-            .padding()
-        }
-    }
-}
-
-struct ButtonLabel: View {
-    let title: String
-    let systemImage: String
+  static var previews: some View {
+    let mix = MeowMix()
     
-    var body: some View {
-        HStack {
-            Spacer()
-            Label(title, systemImage: systemImage)
-            Spacer()
-        }
-        .padding(.vertical)
-        .background(Color.gray.opacity(0.15))
-        .cornerRadius(12)
-    }
+    ContentView()
+    
+    FeaturedCats(artists: mix.tracks.map(\.artist))
+      .previewLayout(.sizeThatFits)
+    
+    TrackRow(track: mix.tracks.first!)
+      .previewLayout(.sizeThatFits)
+    
+    FeaturedArtist(artist: "Keyboard Cat")
+      .previewLayout(.sizeThatFits)
+  }
 }
 
-struct Buttons: View {
-    var body: some View {
-        HStack(spacing: 12.0) {
-            Button { print("Play!") }
-                label: {
-                    ButtonLabel(title: "Play", systemImage: "play.fill")
-                }
-            
-            Button { print("Shuffle!") }
-                label: {
-                    ButtonLabel(title: "Shuffle", systemImage: "shuffle")
-                }
-        }
+struct TrackRow: View {
+  let track: Track
+  
+  var body: some View {
+    HStack {
+      track.thumbnail
+        .padding()
+        .background(track.gradient)
+        .cornerRadius(6)
+      
+      Text(track.title)
+      Text(track.artist)
+        .foregroundColor(.secondary)
+        .lineLimit(1)
+        .truncationMode(.tail)
+      
+      Spacer()
+      
+      Text("\(track.duration)")
     }
+  }
 }
 
-struct MixDescription: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 20.0) {
-            VStack(alignment: .leading) {
-                Text("Meow! Mix")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Text("Apple Music for Ozma")
-                    .font(.title)
-                    .fontWeight(.light)
-                    .foregroundColor(.accentColor)
-                Text("Updated Caturday")
-                    .font(Font.system(.body).smallCaps())
-            }
-            
-            Text("Whether you're a kitten or an old-timer, get meowing with this purrsonalized mix of music to meow to!")
-        }
+struct FeaturedArtist: View {
+  let artist: String
+  
+  var body: some View {
+    VStack {
+      ZStack {
+        Circle()
+          .fill([Color.orange, .pink, .purple, .red, .yellow].randomElement()!)
+          .scaledToFit()
+        
+        Image(systemName: "music.mic")
+          .resizable()
+          .frame(width: 50, height: 50)
+          .foregroundColor(.white)
+      }
+      
+      Text(artist)
+        .multilineTextAlignment(.center)
+        .lineLimit(2)
     }
+    .frame(width: 120)
+  }
 }
