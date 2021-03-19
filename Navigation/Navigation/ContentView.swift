1cda51baf3545680e66520a383c6a4d663a448e6
eonsubae <eonsubae@naver.com>
2021-03-19 09:01:54 +0900

    Shift another view

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -4,10 +4,19 @@ struct ContentView: View {
   @State var artworks = artData
   
   var body: some View {
+    NavigationView {
       List(artworks) { artwork in
-          Text(artwork.title)
+          NavigationLink(
+            destination: DetailView(artwork: artwork),
+            label: {
+                Text(artwork.title)
+            })
       }
       .listStyle(PlainListStyle())
+      .navigationTitle("Artworks")
+        
+      DetailView(artwork: artworks[0])
+    }
   }
 }
 
