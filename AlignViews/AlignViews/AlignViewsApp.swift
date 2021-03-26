69d69d05ca55cb9d125bc8d947fc2727619d106a
eonsubae <eonsubae@naver.com>
2021-03-26 22:10:03 +0900

    Stack Alignment

---
+++ AlignViews/AlignViews/AlignViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignViewsApp.swift
+//  AlignViews
+//
+//  Created by KRENGLSSEAN on 2021/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AlignViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlignViews/AlignViews/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack(alignment: .lastTextBaseline) {
+            Image("Cake VStack")
+                .resizable()
+                .scaledToFit()
+            Text("Hello, Layout!")
+            Image("Pancake VStack")
+                .resizable()
+                .frame(width: 100.0, height: 100.0)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
