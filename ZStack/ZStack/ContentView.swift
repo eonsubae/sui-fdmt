9c1f74985c39f73b29df426d27f492ae5e224e79
eonsubae <eonsubae@naver.com>
2021-04-04 11:59:56 +0900

    ZStack

---
+++ ZStack/ZStack/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  ZStack
+//
+//  Created by KRENGLSSEAN on 2021/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let gradient = LinearGradient(
+        gradient: Gradient(
+            stops: [
+                .init(color: .brightSeafoam, location: CGFloat(3.0 / 8)),
+                .init(color: .darkSeafoam, location: 1)
+            ]
+        ), startPoint: .topTrailing, endPoint: .bottom)
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+          Image("RWStack")
+            .resizable()
+            .scaledToFit()
+            
+          gradient
+            .blendMode(.multiply)
+            .layoutPriority(-1)
+
+          VStack(alignment: .leading) {
+            Image("ZStack")
+              .resizable()
+              .scaledToFit()
+              .frame(width: 80)
+
+            Text("Stacks to the Max!")
+              .fontWeight(.semibold)
+                .foregroundColor(.brightSeafoam)
+          }
+          .alignmentGuide(HorizontalAlignment.center) { $0[.trailing] }
+          .alignmentGuide(VerticalAlignment.center) { $0[.top] }
+        }
+    }
+}
+
+extension Color {
+    static let brightSeafoam = Color("Bright Seafoam")
+    static let darkSeafoam = Color("Dark Seafoam")
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZStack/ZStack/ZStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZStackApp.swift
+//  ZStack
+//
+//  Created by KRENGLSSEAN on 2021/04/04.
+//
+
+import SwiftUI
+
+@main
+struct ZStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
