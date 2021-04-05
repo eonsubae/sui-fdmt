d4b8e5657b12201409b83e737b0f9d1ef75746b2
eonsubae <eonsubae@naver.com>
2021-04-05 22:02:02 +0900

    GeometryReader

---
+++ GeometryReader/GeometryReader/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  GeometryReader
+//
+//  Created by KRENGLSSEAN on 2021/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        GeometryReader { proxy in
+            HStack {
+                Image("Cake VStack")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.5)
+                Text("Reading is dreaming with open eyes.")
+                    .layoutPriority(1)
+                Image("Pancake VStack")
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeometryReader/GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by KRENGLSSEAN on 2021/04/05.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
