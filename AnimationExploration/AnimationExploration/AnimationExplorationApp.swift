285f6eb2e873b67379e9abc3d847a90e732bf39b
eonsubae <eonsubae@naver.com>
2021-04-07 22:10:33 +0900

    Animation

---
+++ AnimationExploration/AnimationExploration/AnimationExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExplorationApp.swift
+//  AnimationExploration
+//
+//  Created by KRENGLSSEAN on 2021/04/07.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationExploration/AnimationExploration/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  AnimationExploration
+//
+//  Created by KRENGLSSEAN on 2021/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private struct AnimationData {
+      let offset: CGSize
+      let color: Color
+
+      static let array: [Self]  = [
+        .init(
+          offset: .init(width: 0, height: 0),
+          color: .green
+        ),
+        .init(
+          offset: .init(width: 100, height: 0),
+          color: .blue
+        ),
+        .init(
+          offset: .init(width: 100, height: -100),
+          color: .red
+        ),
+        .init(
+          offset: .init(width: -100, height: -100),
+          color: .orange
+        ),
+        .init(
+          offset: .init(width: -100, height: 0),
+          color: .yellow
+        )
+      ]
+    }
+    
+    @State private var animationData = AnimationData.array[0]
+    
+    var body: some View {
+        Circle()
+            .scaleEffect(0.5)
+            .foregroundColor(animationData.color)
+            .animation(.default)
+            .offset(animationData.offset)
+            .padding()
+            .onAppear {
+                for (index, data) in AnimationData.array.enumerated().dropFirst() {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(index)) {
+                        animationData = data
+                    }
+                }
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
