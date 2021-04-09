06ea76b74c28d5acdaf502136e7a7c4ee49f85e2
eonsubae <eonsubae@naver.com>
2021-04-09 21:13:30 +0900

    Spinner

---
+++ Spinner/Spinner/SpinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinnerApp.swift
+//  Spinner
+//
+//  Created by KRENGLSSEAN on 2021/04/09.
+//
+
+import SwiftUI
+
+@main
+struct SpinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SpinnerView()
+        }
+    }
+}
---
+++ Spinner/Spinner/SpinnerView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Spinner
+//
+//  Created by KRENGLSSEAN on 2021/04/09.
+//
+
+import SwiftUI
+
+struct SpinnerView: View {
+  struct Leaf: View {
+    let rotation: Angle
+    let isCurrent: Bool
+    
+    var body: some View {
+      Capsule()
+        .stroke(isCurrent ? Color.white : .gray, lineWidth: 8)
+        .frame(width: 20, height: 50)
+//        .offset(x: 0, y: 70)
+        .offset(
+          isCurrent
+            ? .init(width: 10, height: 0)
+            : .init(width: 40, height: 70)
+        )
+        .scaleEffect(isCurrent ? 0.5 : 1)
+        .rotationEffect(rotation)
+        .animation(.easeIn(duration: 1.5))
+    }
+  }
+  
+  let leavesCount = 12
+  @State var currentIndex = -1
+  
+  var body: some View {
+    VStack {
+      ZStack {
+        ForEach(0 ..< leavesCount) { index in
+            Leaf(
+                rotation: .init(degrees: .init(index) / .init(leavesCount) * 360),
+                isCurrent: index == currentIndex
+            )
+        }
+      }
+      .onAppear(perform: animate)
+    }
+  }
+  
+  func animate() {
+    Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) { timer in
+        currentIndex = (currentIndex + 1) % leavesCount
+    }
+  }
+}
+
+struct SpinnerView_Previews : PreviewProvider {
+  static var previews: some View {
+    SpinnerView()
+  }
+}
+
